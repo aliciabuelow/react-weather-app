06cb21fa21abd9199758bb53be9acda23d61bb02
Alicia Buelow <alicia@buelow.com.au>
2026-02-28 11:31:13 +0800

    Added footer with link to App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,5 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  return <h1>React Weather App</h1>
+  return (
+    <div className="App">
+    <h1>React Weather App</h1>
+    <footer>Coded by Alicia Buelow and open-sourced <a href="https://github.com/aliciabuelow/react-weather-app" target="_blank">on GitHub</a></footer>
+    </div>
+    )
 }
