eb23ed178a2ca062b7026e4a636cafe2af72d359
Alicia Buelow <alicia@buelow.com.au>
2026-02-28 14:20:16 +0800

    Installed axios and integrated API

--- my-react-app/src/Weather.jsx
+++ my-react-app/src/Weather.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import './Weather.css'
+import axios from 'axios';
 
 export default function Weather() {
-    return (
+    const [isReady, setIsReady] = useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+    function handleResponse(response) {
+        setWeatherData({
+            city: response.data.city,
+            country: response.data.country,
+            description: response.data.condition.description,
+            icon: response.data.condition.icon_url,
+            temperature: response.data.temperature.current,
+            humidity: response.data.temperature.humidity,
+            wind: response.data.wind.speed
+        })
+        setIsReady(true)
+    }
+
+    if (isReady) {
+        return (
         <div className="weather mt-5">
             <form className="search-element mb-4">
                 <input type="search" placeholder="Enter city" className="search-input" autoFocus required />
@@ -10,33 +29,40 @@ export default function Weather() {
 
             <div className="weather-content row">
                 <div className="col-8 text-start">
-                    <img src="https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg" alt="weather icon"/>
+                    <img src={weatherData.icon} alt={weatherData.description} />
                     <span className="temperature">
-                        26
+                        {Math.round(weatherData.temperature)}
                     </span>
                     <span className="temperature-units">
                         °C|°F
                     </span>
                     <span className="weather-details">
                         <ul>
-                            <li>Precipitation: 0%</li>
-                            <li>Humidity: 48%</li>
-                            <li>Wind: 26 km/h</li>
+                            <li>Humidity: {weatherData.humidity}%</li>
+                            <li>Wind: {Math.round(weatherData.wind)} km/h</li>
                         </ul>
                     </span>
                 </div>
                 <div className="col-4 text-end">
                     <div className="city">
-                        Perth
+                        {weatherData.city}
                     </div>
                     <div className="city-timestamp">
                         Saturday 12:00 pm
                     </div>
                     <div className="condition">
-                        Sunny
+                        {weatherData.description}
                     </div>
                 </div>
             </div>
         </div>
     )
+    }
+ else {
+    const apiKey = "04dbc8004716437tab5bc0bfo1baf277";
+    let defaultCity = "Perth";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..."
+ }
 }
